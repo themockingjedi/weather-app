2cb6ae9106beb5318b10eecabcb6a4678a8e89f1
DeAnne Lombardi <64880045+themockingjedi@users.noreply.github.com>
2022-01-23 15:41:40 -0500

    removed html to js for injecting multiple times

--- src/app.js
+++ src/app.js
@@ -21,6 +21,32 @@ function formalDate(timestamp) {
   return `Last updated: ${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weekly-forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day)) {
+
+      forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+      <div class="forecast-date">${day}</div>
+      <img src="http://openweathermap.org/img/wn/50d@2x.png"
+      alt=""
+      width="42"
+      />
+      <div class="forecast-temps">
+      <span class="max-temp"> 35 </span>
+      <span class="min-temp"> 22 </span>
+      </div>
+      </div>`;
+      
+      
+      forecastHTML = forecastHTML + `</div>`;
+      forecastElement.innerHTML = forecastHTML;
+    }
+}
+
 function displayTemperature(response) {
   let temp = document.querySelector("#temp");
   temp.innerHTML = Math.round(response.data.main.temp);
@@ -93,3 +119,4 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 search("Boston");
+displayForecast();
